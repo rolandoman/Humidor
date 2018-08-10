e9f142db8849e5e5aa1a5ac6ce2fbbcf1086c483
SuperVisor Standard account <roland.clarke@gmail.com>
2018-08-10 11:45:59 -0700

    Added some more logic to the install script

--- HCH_STABLE_080818/INSTALL.sh
+++ HCH_STABLE_080818/INSTALL.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-mkdir source
-cat HCH_STABLE_080818.ino aa_definitions.h *.ino > source/source.ino
+if [ ! -d "source" ]; then
+        mkdir source
+fi
+
+rm -rf source/*
+
+pname=`basename "$PWD"`
+cat $pname.ino aa_definitions.h *.ino > source/source.ino
+
 cp Makefile source/
 cd source
 make
