52ffff98416d9c599e6c2b124b75babaa3cdb56f
SuperVisor Standard account <roland.clarke@gmail.com>
2018-08-10 12:03:23 -0700

    reorganization, cleaned up file locations

--- HCH_STABLE_080818/INSTALL.sh
+++ HCH_STABLE_080818/INSTALL.sh
@@ -6,8 +6,10 @@ fi
 
 rm -rf source/*
 
+cd raw
+
 pname=`basename "$PWD"`
-cat $pname.ino aa_definitions.h *.ino > source/source.ino
+cat $pname.ino aa_definitions.h *.ino > ../source/source.ino
 
 cp Makefile source/
 cd source
