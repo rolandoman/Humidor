e1b8427827c0f2d84938f0bfa27a94b98a74bdb1
unknown <roland.clarke@gmail.com>
2018-08-10 17:54:09 -0700

    major edits to fix the http post function

--- HCH_STABLE_100818/raw/g_comms.ino
+++ HCH_STABLE_100818/raw/g_comms.ino
@@ -11,56 +11,56 @@ void sendData() {
   // if there's a successful connection:
   if (eclient.connect("rmclarke.ca", 8086)) {
 
-    char outBuf[4];
-    char databuffer[3];
-    char data[1024]; // need to end reliance on arduino 'Strings'
+    char outBuf[30];
+    char databuffer[6];
+    char data[512]; // need to end reliance on arduino 'Strings'
 
     //String data = String("temp1,owner=rolo,group=home,device=hch2 value=")+String(Tb1,DEC)+"."+String(Tb2,DEC)+"\n";
     strcpy(data, "temp1,owner=rolo,group=home,device=hch2 value=");
-    snprintf(databuffer, 2, "%02d", Tb1);strcat(data, databuffer);strcat(data, ".");
-    snprintf(databuffer, 2, "%02d", Tb2);strcat(data, databuffer);strcat(data, "\n");
+    snprintf(databuffer, 3, "%02d", Tb1);strcat(data, databuffer);strcat(data, ".");
+    snprintf(databuffer, 3, "%02d", Tb2);strcat(data, databuffer);strcat(data, "\n");
 
     Tb1 = (unsigned int) (curT2 / 100);
     Tb2 = (unsigned int) ((unsigned int)curT2 - ((unsigned int)Tb1 * 100)) ;
     //data.concat(String("temp2,owner=rolo,group=home,device=hch2 value=")+String(Tb1,DEC)+"."+String(Tb2,DEC)+"\n");
     strcat(data, "temp2,owner=rolo,group=home,device=hch2 value=");
-    snprintf(databuffer, 2, "%02d", Tb1);strcat(data, databuffer);strcat(data, ".");
-    snprintf(databuffer, 2, "%02d", Tb2);strcat(data, databuffer);strcat(data, "\n");
+    snprintf(databuffer, 3, "%02d", Tb1);strcat(data, databuffer);strcat(data, ".");
+    snprintf(databuffer, 3, "%02d", Tb2);strcat(data, databuffer);strcat(data, "\n");
 
     //data.concat(String("hum1,owner=rolo,group=home,device=hch2 value=")+String(curH1,DEC)+"\n");
     strcat(data, "hum1,owner=rolo,group=home,device=hch2 value=");
-    snprintf(databuffer, 2, "%02d", curH1);strcat(data, databuffer);strcat(data, "\n");
+    snprintf(databuffer, 3, "%02d", curH1);strcat(data, databuffer);strcat(data, "\n");
 
     //data.concat(String("hum2,owner=rolo,group=home,device=hch2 value=")+String(curH2,DEC)+"\n");
     strcat(data, "hum2,owner=rolo,group=home,device=hch2 value=");
-    snprintf(databuffer, 2, "%02d", curH2);strcat(data, databuffer);strcat(data, "\n");
+    snprintf(databuffer, 3, "%02d", curH2);strcat(data, databuffer);strcat(data, "\n");
 
     //data.concat(String("heatcool,owner=rolo,group=home,device=hch2 value=")+String(heatcoolFlag,DEC)+"\n");
     strcat(data, "heatcool,owner=rolo,group=home,device=hch2 value=");
-    snprintf(databuffer, 1, "%d", heatcoolFlag);strcat(data, databuffer);strcat(data, "\n");
+    snprintf(databuffer, 2, "%d", heatcoolFlag);strcat(data, databuffer);strcat(data, "\n");
 
     //data.concat(String("mist,owner=rolo,group=home,device=hch2 value=")+String(mistFlag,DEC)+"\n");
     strcat(data, "mist,owner=rolo,group=home,device=hch2 value=");
-    snprintf(databuffer, 1, "%d", mistFlag);strcat(data, databuffer);strcat(data, "\n");
+    snprintf(databuffer, 2, "%d", mistFlag);strcat(data, databuffer);strcat(data, "\n");
 
     //data.concat(String("hpower,owner=rolo,group=home,device=hch2 value=")+String(heaterPower,DEC)+"\n");
     strcat(data, "hpower,owner=rolo,group=home,device=hch2 value=");
-    snprintf(databuffer, 3, "%03d", heaterPower);strcat(data, databuffer);strcat(data, "\n");
+    snprintf(databuffer, 4, "%03d", heaterPower);strcat(data, databuffer);strcat(data, "\n");
 
     //data.concat(String("light,owner=rolo,group=home,device=hch2 value=")+String(lightFlag,DEC)+"\n");
     strcat(data, "light,owner=rolo,group=home,device=hch2 value=");
-    snprintf(databuffer, 1, "%d", lightFlag);strcat(data, databuffer);strcat(data, "\n");
+    snprintf(databuffer, 2, "%d", lightFlag);strcat(data, databuffer);strcat(data, "\n");
 
     Tb1 = (unsigned int) (setT / 100);
     Tb2 = (unsigned int) ((unsigned int)setT - ((unsigned int)Tb1 * 100)) ;
     //data.concat(String("setT,owner=rolo,group=home,device=hch2 value=")+String(Tb1,DEC)+"."+String(Tb2,DEC)+"\n");
     strcat(data, "setT,owner=rolo,group=home,device=hch2 value=");
-    snprintf(databuffer, 2, "%02d", Tb1);strcat(data, databuffer);strcat(data, ".");
-    snprintf(databuffer, 2, "%02d", Tb2);strcat(data, databuffer);strcat(data, "\n");
+    snprintf(databuffer, 3, "%02d", Tb1);strcat(data, databuffer);strcat(data, ".");
+    snprintf(databuffer, 3, "%02d", Tb2);strcat(data, databuffer);strcat(data, "\n");
 
     //data.concat(String("setH,owner=rolo,group=home,device=hch2 value=")+String(setH,DEC)+"\n");
     strcat(data, "setH,owner=rolo,group=home,device=hch2 value=");
-    snprintf(databuffer, 2, "%02d", setH);strcat(data, databuffer);strcat(data, "\n");
+    snprintf(databuffer, 3, "%02d", setH);strcat(data, databuffer);strcat(data, "\n");
 
     eclient.println(F("POST /write?db=iot1&u=iot&p=iot HTTP/1.1"));
     eclient.println(F("Host: rmclarke.ca"));
