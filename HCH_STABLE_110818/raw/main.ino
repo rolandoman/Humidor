0df35e6ad382e75d4084d593d21917119a116449
unknown <roland.clarke@gmail.com>
2018-08-11 09:54:07 -0700

    restore menus totally. very odd behavior is gone for now...

--- HCH_STABLE_110818/raw/main.ino
+++ HCH_STABLE_110818/raw/main.ino
@@ -6,22 +6,22 @@ void loop() {
 
   if (topB.wasPressed()) {  //If the button was pressed, rotate runMode
     runMode++;
-    if (runMode>2) {
+    if (runMode>maxRunMode) {
       runMode=0;
     }
     lcd.clear();
     updateLCD();
   }
-  //if (runMode==1) { // Edit the setT of the feedback routine
-  //  if (midB.wasPressed()) {setT = setT + 10;updateLCD();}
-  //  if (botB.wasPressed()) {setT = setT - 10;updateLCD();}
-  //}  else if (runMode==2) { // Edit the setH of the feedback routine
-  //  if (midB.wasPressed()) {setH = setH + 1;updateLCD();}
-  //  if (botB.wasPressed()) {setH = setH - 1;updateLCD();}
-  //}
+  if (runMode==1) { // Edit the setT of the feedback routine
+    if (midB.wasPressed()) {setT = setT + 10;updateLCD();}
+    if (botB.wasPressed()) {setT = setT - 10;updateLCD();}
+  }  else if (runMode==2) { // Edit the setH of the feedback routine
+    if (midB.wasPressed()) {setH = setH + 1;updateLCD();}
+    if (botB.wasPressed()) {setH = setH - 1;updateLCD();}
+  }
 
   //eclient.flush();
-  if (runMode>2) {runMode=0;} // very odd behavior where runMode = 180???
+  if (runMode>maxRunMode) {runMode=0;} // very odd behavior where runMode = 180???
   timer.run();  // timer initiation...
   wdt_reset();
 } // end main loop
