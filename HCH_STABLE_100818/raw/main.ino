efc55edd44950ec4242a56c3cb325906b415123d
unknown <roland.clarke@gmail.com>
2018-08-10 19:17:32 -0700

    minor

--- HCH_STABLE_100818/raw/main.ino
+++ HCH_STABLE_100818/raw/main.ino
@@ -4,7 +4,7 @@ void loop() {
   midB.read();
   botB.read();
 
-  if (topB.wasPressed()) {runMode = runMode+1;if (runMode>2) {runMode=0;}lcd.clear();updateLCD();}  //If the button was pressed, say so...
+  if (topB.wasPressed()) {runMode = runMode+1;if (runMode>2) {runMode=0;};lcd.clear();updateLCD();}  //If the button was pressed, say so...
   if (runMode==1) { // Edit the setT of the feedback routine
     if (midB.wasPressed()) {setT = setT + 10;updateLCD();}
     if (botB.wasPressed()) {setT = setT - 10;updateLCD();}
