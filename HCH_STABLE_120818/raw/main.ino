89c25c9504ddd20b57ec11324f0d09bfbacb35cf
unknown <roland.clarke@gmail.com>
2018-08-12 08:41:05 -0700

    minor edit

--- HCH_STABLE_120818/raw/main.ino
+++ HCH_STABLE_120818/raw/main.ino
@@ -12,21 +12,22 @@ void loop() {
   }
   if (runMode==1) { // Edit the setT of the feedback routine
     if (midB.wasPressed()) {
-      setT = setT + 10;
-      updateLCD();}
+      setT = setT + 10;updateLCD();
+    }
     if (botB.wasPressed()) {
-      setT = setT - 10;
-      updateLCD();}
+      setT = setT - 10;updateLCD();
+    }
   }  else if (runMode==2) { // Edit the setH of the feedback routine
     if (midB.wasPressed()) {
-      setH = setH + 1;
-      updateLCD();}
+      setH = setH + 1;updateLCD();
+    }
     if (botB.wasPressed()) {
-      setH = setH - 1;
-      updateLCD();}
+      setH = setH - 1;updateLCD();
+    }
   } else if (runMode==3) {
     if (midB.wasPressed() || botB.wasPressed()) {
       if (fruitFlag) {fruitFlag=false;} else {fruitFlag=true;}
+      updateLCD();
     }
   }
 
