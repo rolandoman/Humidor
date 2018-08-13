36e6518e87ac5daa3332e6b5d072f3e9f6e8ed4b
unknown <roland.clarke@gmail.com>
2018-08-13 16:07:59 -0700

    complete the readEEPROMS and updateEEPROMS functions

--- HCH_STABLE_120818/raw/a_EEPROMS.ino
+++ HCH_STABLE_120818/raw/a_EEPROMS.ino
@@ -1,3 +1,22 @@
-void readEEPROMS() {}
+void readEEPROMS() {
+  int eeAddress = 0;
 
-voide updateEEPROMS() {}
+  EEPROM.get(eeAddress, setT);
+  eeAddress += sizeof(setT);
+  EEPROM.get(eeAddress, setH);
+  eeAddress += sizeof(setH);
+  EEPROM.get(eeAddress, fruitFlag);
+  eeAddress += sizeof(fruitFlag);
+}
+
+void updateEEPROMS() {
+  int eeAddress = 0;
+
+  EEPROM.put(eeAddress, setT);
+  eeAddress += sizeof(setT);
+  EEPROM.put(eeAddress, setH);
+  eeAddress += sizeof(setH);
+  EEPROM.put(eeAddress, fruitFlag);
+  eeAddress += sizeof(fruitFlag);
+
+}
