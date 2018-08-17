7f3281b542da68cc372aaeae99dcec4e3d443fff
unknown <roland.clarke@gmail.com>
2018-08-17 16:55:16 -0700

    syntax

--- HCH_STABLE_160818/raw/e_time.ino
+++ HCH_STABLE_160818/raw/e_time.ino
@@ -108,7 +108,7 @@ void isDaytime () {
     char s_otp[10];
     //lcd.setCursor(5,1);lcd.print(dev_otp);
     if (eclient.find((char *)"\r\niot: ")) {
-      lcd.setCursor(5,1);lcd.print(F("iotBABY ");
+      lcd.setCursor(5,1);lcd.print(F("iotBABY "));
       // OK, server is communicating, parse the message
       // this should work no matter what order the variables are received
       //if (eclient.find((char *)"dev=")) {
