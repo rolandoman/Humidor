024ed2eb5d50a5a88be5c6b0f156dbba9f84c884
unknown <roland.clarke@gmail.com>
2018-08-17 16:39:13 -0700

    roll back all my friggen comms because grafana not getting data

--- HCH_STABLE_160818/raw/e_time.ino
+++ HCH_STABLE_160818/raw/e_time.ino
@@ -99,58 +99,58 @@ void isDaytime () {
     } //else {Serial.println(F("No Date"));}
 
     // still inside the request - here is where we parse the rest of the header
-    char buf2[20];char temp[20];
-    char s_dev[5];
-    unsigned int s_setT;
-    unsigned char s_setH;
-    boolean s_fruit;
-    unsigned long s_lastupdate;
-    char s_otp[10];
-    if (eclient.find((char *)"\r\niot: ")) {
+    //char buf2[20];char temp[20];
+    //char s_dev[5];
+    //unsigned int s_setT;
+    //unsigned char s_setH;
+    //boolean s_fruit;
+    //unsigned long s_lastupdate;
+    //char s_otp[10];
+    //if (eclient.find((char *)"\r\niot: ")) {
       // OK, server is communicating, parse the message
       // this should work no matter what order the variables are received
-      if (eclient.find((char *)"dev=")) {
-        eclient.readBytes(buf2, 4);
-        snprintf(s_dev,6,"%s",buf2);
-      }
-      if (eclient.find((char *)"setT=")) {
-        eclient.readBytes(buf2, 4);
-        snprintf(temp,6,"%s",buf2);
-        s_setT = (unsigned int) strtoul (temp, NULL, 0);
-      }
-      if (eclient.find((char *)"setH=")) {
-        eclient.readBytes(buf2, 2);
-        snprintf(temp,4,"%s",buf2);
-        s_setH = (unsigned char) strtoul (temp, NULL, 0);
-      }
-      if (eclient.find((char *)"fruit=")) {
-        eclient.readBytes(buf2, 1);
-        snprintf(temp,3,"%s",buf2);
-        s_fruit = (boolean) strtoul (temp, NULL, 0);
-      }
-      if (eclient.find((char *)"lastupdate=")) {
-        eclient.readBytes(buf2, 10);
-        snprintf(temp,12,"%s",buf2);
-        s_lastupdate = strtoul (temp, NULL, 0);
-      }
-      if (eclient.find((char *)"otp=")) {
-        eclient.readBytes(buf2, 8);
-        snprintf(s_otp,10,"%s",buf2);
-      }
+      //if (eclient.find((char *)"dev=")) {
+      //  eclient.readBytes(buf2, 4);
+      //  snprintf(s_dev,6,"%s",buf2);
+      //}
+      //if (eclient.find((char *)"setT=")) {
+      //  eclient.readBytes(buf2, 4);
+      //  snprintf(temp,6,"%s",buf2);
+      //  s_setT = (unsigned int) strtoul (temp, NULL, 0);
+      //}
+      //if (eclient.find((char *)"setH=")) {
+      //  eclient.readBytes(buf2, 2);
+      //  snprintf(temp,4,"%s",buf2);
+      //  s_setH = (unsigned char) strtoul (temp, NULL, 0);
+      //}
+      //if (eclient.find((char *)"fruit=")) {
+      //  eclient.readBytes(buf2, 1);
+      //  snprintf(temp,3,"%s",buf2);
+      //  s_fruit = (boolean) strtoul (temp, NULL, 0);
+      //}
+      //if (eclient.find((char *)"lastupdate=")) {
+      //  eclient.readBytes(buf2, 10);
+      //  snprintf(temp,12,"%s",buf2);
+      //  s_lastupdate = strtoul (temp, NULL, 0);
+      //}
+      //if (eclient.find((char *)"otp=")) {
+      //  eclient.readBytes(buf2, 8);
+      //  snprintf(s_otp,10,"%s",buf2);
+      //}
 
       // cpmfirm good comms, then compare who's lastupdate is bigger, then make a choice
-      if ((dev_otp == s_otp) && (device == s_dev) && (lastupdate < s_lastupdate)) {
+      //if ((dev_otp == s_otp) && (device == s_dev) && (lastupdate < s_lastupdate)) {
         // server wins, update my shit and save it
-        lastupdate = s_lastupdate;
-        setT = s_setT;setH = s_setH;
-        fruitFlag = s_fruit;
-        updateEEPROM();
+      //  lastupdate = s_lastupdate;
+      //  setT = s_setT;setH = s_setH;
+      //  fruitFlag = s_fruit;
+      //  updateEEPROM();
 
-      } else {
+      //} else {
         // device wins, do nothing, hope that the server updates its shit
-      }
+      //}
 
-    } // else { no iot found }
+    //} // else { no iot found }
 
     netFlag = true;
   } else {
