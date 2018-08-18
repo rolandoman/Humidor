79da2a4d0833a549a37d05395212fc3aeda8fa15
unknown <roland.clarke@gmail.com>
2018-08-18 12:30:33 -0700

    take out snprintf, replace with sprintf. function seems to create no end of grief

--- HCH_STABLE_180818/raw/g_comms.ino
+++ HCH_STABLE_180818/raw/g_comms.ino
@@ -10,45 +10,44 @@ void sendData() {
   unsigned char Tb2 = (unsigned char) ((unsigned int)curT1 - ((unsigned int)Tb1 * 100));
 
   strcpy(data, "temp1,owner=rolo,group=home,device=hch2 value=");
-  snprintf(databuffer, 3, "%02d", Tb1);strcat(data, databuffer);strcat(data, ".");
-  snprintf(databuffer, 3, "%02d", Tb2);strcat(data, databuffer);strcat(data, "\n");
+  sprintf(databuffer, "%02d", Tb1);strcat(data, databuffer);strcat(data, ".");
+  sprintf(databuffer, "%02d", Tb2);strcat(data, databuffer);strcat(data, "\n");
 
   Tb1 = (unsigned char) (curT2 / 100);
   Tb2 = (unsigned char) ((unsigned int)curT2 - ((unsigned int)Tb1 * 100)) ;
   strcat(data, "temp2,owner=rolo,group=home,device=hch2 value=");
-  snprintf(databuffer, 3, "%02d", Tb1);strcat(data, databuffer);strcat(data, ".");
-  snprintf(databuffer, 3, "%02d", Tb2);strcat(data, databuffer);strcat(data, "\n");
+  sprintf(databuffer, "%02d", Tb1);strcat(data, databuffer);strcat(data, ".");
+  sprintf(databuffer, "%02d", Tb2);strcat(data, databuffer);strcat(data, "\n");
 
   strcat(data, "hum1,owner=rolo,group=home,device=hch2 value=");
-  snprintf(databuffer, 3, "%02d", curH1);strcat(data, databuffer);strcat(data, "\n");
+  sprintf(databuffer, "%02d", curH1);strcat(data, databuffer);strcat(data, "\n");
 
   strcat(data, "hum2,owner=rolo,group=home,device=hch2 value=");
-  snprintf(databuffer, 3, "%02d", curH2);strcat(data, databuffer);strcat(data, "\n");
+  sprintf(databuffer, "%02d", curH2);strcat(data, databuffer);strcat(data, "\n");
 
   strcat(data, "heatcool,owner=rolo,group=home,device=hch2 value=");
-  snprintf(databuffer, 3, "%d", heatcoolFlag);strcat(data, databuffer);strcat(data, "\n");
+  sprintf(databuffer, "%d", heatcoolFlag);strcat(data, databuffer);strcat(data, "\n");
 
   strcat(data, "mist,owner=rolo,group=home,device=hch2 value=");
-  snprintf(databuffer, 3, "%d", mistFlag);strcat(data, databuffer);strcat(data, "\n");
+  sprintf(databuffer, "%d", mistFlag);strcat(data, databuffer);strcat(data, "\n");
 
   strcat(data, "hpower,owner=rolo,group=home,device=hch2 value=");
-  snprintf(databuffer, 5, "%03d", heaterPower);strcat(data, databuffer);strcat(data, "\n");
+  sprintf(databuffer, "%03d", heaterPower);strcat(data, databuffer);strcat(data, "\n");
 
   strcat(data, "light,owner=rolo,group=home,device=hch2 value=");
-  snprintf(databuffer, 3, "%d", lightFlag);strcat(data, databuffer);strcat(data, "\n");
+  sprintf(databuffer, "%d", lightFlag);strcat(data, databuffer);strcat(data, "\n");
 
   Tb1 = (unsigned char) (setT / 100);
   Tb2 = (unsigned char) ((unsigned int)setT - ((unsigned int)Tb1 * 100)) ;
   strcat(data, "setT,owner=rolo,group=home,device=hch2 value=");
-  snprintf(databuffer, 3, "%02d", Tb1);strcat(data, databuffer);strcat(data, ".");
-  snprintf(databuffer, 3, "%02d", Tb2);strcat(data, databuffer);strcat(data, "\n");
+  sprintf(databuffer, "%02d", Tb1);strcat(data, databuffer);strcat(data, ".");
+  sprintf(databuffer, "%02d", Tb2);strcat(data, databuffer);strcat(data, "\n");
 
   strcat(data, "setH,owner=rolo,group=home,device=hch2 value=");
-  snprintf(databuffer, 3, "%02d", setH);strcat(data, databuffer);strcat(data, "\n");
+  sprintf(databuffer, "%02d", setH);strcat(data, databuffer);strcat(data, "\n");
 
   udp.beginPacket(influxserver, udpport);
   udp.print(data);
   udp.endPacket();
-
 
 }
