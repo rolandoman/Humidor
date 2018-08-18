ac175d30e5f10e8e9bdfe6d0fced1e9ec862e99d
unknown <roland.clarke@gmail.com>
2018-08-18 12:33:22 -0700

    change setT parser back to %d

--- HCH_STABLE_180818/raw/e_time.ino
+++ HCH_STABLE_180818/raw/e_time.ino
@@ -118,7 +118,7 @@ void isDaytime () {
       }
       if (eclient.find((char *)"setT=")) {
         eclient.readBytes(buf2, 4);
-        sprintf(temp,"%u",buf2);
+        sprintf(temp,"%d",buf2);
         s_setT = (unsigned int) strtoul (temp, NULL, 0);
       }
       //if (eclient.find((char *)"setH=")) {
