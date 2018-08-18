9f5563d99eb39e3da9d2dbdbfec7256af1c74c94
unknown <roland.clarke@gmail.com>
2018-08-18 15:19:45 -0700

    add back the code for comparing the two strings

--- HCH_TESTING_180818/raw/e_time.ino
+++ HCH_TESTING_180818/raw/e_time.ino
@@ -154,8 +154,7 @@ void isDaytime () {
       //} else {
         // device wins, do nothing, hope that the server updates its shit
       //}
-      //if ((strcmp(dev_otp,s_otp) == 0) && (strcmp(device,s_dev) == 0)) {
-      if (strcmp(dev_otp,s_otp) == 0) {
+      if ((strcmp(dev_otp,s_otp) == 0) && (strcmp(device,s_dev) == 0)) {
         lcd.setCursor(9,1);lcd.print(F("iot"));
       }
     } // else { no iot found }
